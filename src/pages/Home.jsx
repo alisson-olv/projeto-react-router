db58b8c35b52d9434c63bc4ab1d778b1947bd467
Alisson Oliveira <binho_alisson@hotmail.com>
2023-06-18 19:58:18 -0300

    utilização de rotas com Link do react-router-dom

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
+
+import './Home.css';
 
 const Home = () => {
+  const url = 'http://localhost:3000/products';
+
+  const { data: items, loading, error } = useFetch(url);
+
   return (
     <div>
       <h2>
         Home
       </h2>
+      <h3>
+        Produtos
+      </h3>
+      {error && <p>Houve um problema ao carregar os dados.</p>}
+      {items && items.map((item) => (
+        <div key={item.id}>
+          <h4>
+            {item.name}
+          </h4>
+          <p>
+            R$ {item.price}
+          </p>
+        </div>
+      ))}
     </div>
   )
 }
